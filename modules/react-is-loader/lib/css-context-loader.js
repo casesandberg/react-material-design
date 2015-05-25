7275df32c414e4abae1480478f29f573c631349b
case <case@casesandberg.com>
2015-05-24 21:06:11 -0700

    remove `is-loader`

--- modules/react-is-loader/lib/css-context-loader.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(file) {
-  var isPropBlock, isPropValue, match;
-  while (match = /("is": "(.+?)")/.exec(file)) {
-    isPropBlock = match[1];
-    isPropValue = match[2];
-    file = file.replace(isPropBlock, '"style": this.styles().' + isPropValue);
-  }
-  return file;
-};
